9e337a9ef79af71b3a7488febe108faae9f85382
MelonMayhem2564 <550727@chrome.escg.ac.uk>
2025-02-10 11:12:54 +0000

    flowers

--- Assets/Scripts/Playerscript.cs
+++ Assets/Scripts/Playerscript.cs
@@ -43,8 +43,8 @@ public class Playerscript: MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 SimpleMoveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(SimpleMoveDir.normalized * speed * Time.deltaTime);
+            Vector3 MoveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(MoveDir.normalized * speed * Time.deltaTime);
             anim.SetBool("Run", true);
         }
         else
